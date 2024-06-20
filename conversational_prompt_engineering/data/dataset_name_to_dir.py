2f0e952675ff6bae78b159186becc54be717ad45
Lena Dankin <lenad@il.ibm.com>
2024-06-20 16:42:17 +0300

    copy datasets from main branch

--- conversational_prompt_engineering/data/dataset_name_to_dir.py
+++ conversational_prompt_engineering/data/dataset_name_to_dir.py
@@ -12,13 +12,17 @@ dataset_name_to_dir = {"Movie Reviews" : {"train":
                                         "desc": "This dataset consists of passages from legal documents \ndiscussing privacy policies or software licenses."},
                        "IBM blog": {"train": "./data/ibm blog/train.csv",
                                                 "eval": "./data/ibm blog/test.csv",
-                                                 "desc": "This dataset consists blog entries from IBM blog."},
+                                                 "desc": "This dataset contains blog entries from IBM blog."},
                        "climate blog": {"train": "./data/climate blog/train.csv",
                                     "eval": "./data/climate blog/test.csv",
                                     "desc": "This dataset consists of articles about climate related issues \npublished in https://www.climaterealityproject.org."},
 
                        "Reddit posts": {"train": "./data/tldr/train.csv",
                                         "eval": "./data/tldr/test.csv",
-                                        "desc": "This dataset consists of posts from Reddit (TL;DR dataset)"}
+                                        "desc": "This dataset consists of posts from Reddit (TL;DR dataset)"},
+
+                       "Restaurant reviews": {"train": "./data/lentricote_trip_advisor/train.csv",
+                                        "eval": "./data/lentricote_trip_advisor/test.csv",
+                                        "desc": "This dataset consists of reviewes of the restaurant\n \"L'entrecote\" in London, posted on the trip-advisor website"}
 
                        }
