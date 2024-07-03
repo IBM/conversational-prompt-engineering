c98f925e8d1da094d0a4789120eea48bb0b84ff0
oritht <oritht@il.ibm.com>
2024-07-03 19:39:23 +0300

    added user mapping

--- conversational_prompt_engineering/backend/output_dir_mapping.py
+++ conversational_prompt_engineering/backend/output_dir_mapping.py
@@ -2,6 +2,8 @@ output_dir_hash_to_name = {
     "74861ab191a8ce2c": "Lena_BAM",
     "64f091a8dbf6be52": "Lena_WatsonX",
     "a134f0dd313b4072": "Shai_BAM",
-    "355633ff21f93197": "Shai_WatsonX"
+    "355633ff21f93197": "Shai_WatsonX",
+    "a0a8d57d8602e844": "Orith_BAM",
+    "65fd14532d5368bd": "Orith_WatsonX"
 
 }
