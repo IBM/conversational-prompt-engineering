1ad585efd243276b3a42626258931a5800632838
artemspector <artem_spector@yahoo.com>
2024-07-04 08:45:07 +0300

    updated dir_mapping

--- conversational_prompt_engineering/backend/output_dir_mapping.py
+++ conversational_prompt_engineering/backend/output_dir_mapping.py
@@ -4,6 +4,6 @@ output_dir_hash_to_name = {
     "a134f0dd313b4072": "Shai_BAM",
     "355633ff21f93197": "Shai_WatsonX",
     "a0a8d57d8602e844": "Orith_BAM",
-    "65fd14532d5368bd": "Orith_WatsonX"
-
+    "65fd14532d5368bd": "Orith_WatsonX",
+    "6435265997232f48": "Artem_WatsonX",
 }
