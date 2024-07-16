8b10a1f465966683a6fb56b7f3fdf433da529a4a
oritht <oritht@il.ibm.com>
2024-07-16 16:33:39 +0300

    add three classification datasets

--- conversational_prompt_engineering/data/main_dataset_name_to_dir.py
+++ conversational_prompt_engineering/data/main_dataset_name_to_dir.py
@@ -23,6 +23,21 @@ dataset_name_to_dir = {"Movie Reviews" : {"train":
 
                        "Restaurant reviews": {"train": "./data/public/lentricote_trip_advisor/train.csv",
                                         "eval": "./data/public/lentricote_trip_advisor/test.csv",
-                                        "desc": "This dataset consists of reviewes of the restaurant\n \"L'entrecote\" in London, posted on the trip-advisor website"}
+                                        "desc": "This dataset consists of reviewes of the restaurant\n \"L'entrecote\" in London, posted on the trip-advisor website"},
+
+                       "20 Newsgroups": {"train": "./data/public/20_newsgroup/train.csv",
+                            "eval": "./data/public/20_newsgroup/test.csv",
+                            "eval_llm": "./data/public/20_newsgroup/test_full.csv",
+                            "desc": "This dataset is a collection newsgroup documents.\nThe topic of the documents is 'space'"},
+
+                       "Consumer Financial Protection Bureau": {"train": "./data/public/cfpb/train.csv",
+                            "eval": "./data/public/cfpb/test.csv",
+                            "eval_llm": "./data/public/cfpb/test_full.csv",
+                            "desc": "This dataset consists of complaints about\nconsumer financial products and services.\nThe topic of the complaints is 'credit reporting'."},
+
+                       "Reuters Financial News": {"train": "./data/public/reuters/train.csv",
+                            "eval": "./data/public/reuters/test.csv",
+                            "eval_llm": "./data/public/reuters/test_full.csv",
+                            "desc": "This dataset is a collection of documents with news articles.\nThe topic of the articles is 'acquisition'."},
 
                        }
