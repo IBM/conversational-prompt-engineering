834cfd7a4481d6de2dad1c7664bb7d3eb103d541
avishaigretz <avishaig@il.ibm.com>
2024-09-16 12:02:24 +0300

    remove BamClient

--- conversational_prompt_engineering/backend/util/llm_clients/llm_clients_loader.py
+++ conversational_prompt_engineering/backend/util/llm_clients/llm_clients_loader.py
@@ -1,10 +1,9 @@
 import os
 from glob import glob
-from conversational_prompt_engineering.backend.util.llm_clients.bam_client import BamClient
 from conversational_prompt_engineering.backend.util.llm_clients.watsonx_client import WatsonXClient
 
 
 def get_client_classes(llm_clients_list):
-    all_models = [BamClient, WatsonXClient]
+    all_models = [WatsonXClient]
     name_to_models = {x.__name__: x for x in all_models}
     return [name_to_models[x] for x in llm_clients_list]
